94d6757687cfedc726f415cbf1f49b14c541991c
JssPlayZ <67428461+JssPlayZ@users.noreply.github.com>
2021-03-11 21:54:59 +0530

    Update Palindrom.java

--- Palindrom.java
+++ Palindrom.java
@@ -9,10 +9,16 @@ class PalindromeProgram
       original = in.nextLine();   
       int length = original.length();   
       for ( int i = length - 1; i >= 0; i-- )  
+      {
          reverse = reverse + original.charAt(i);  
+      }
       if (original.equals(reverse))  
-         System.out.println("Entered string/number is a palindrome.");  
+      {
+         System.out.println("Entered string/number is a palindrome."); 
+      }
       else  
+      {
          System.out.println("Entered string/number isn't a palindrome.");   
+      }
    }  
 }
